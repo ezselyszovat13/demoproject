9d59939f1143dfc72aa02999d18e0a2ec1c92b8a
Ézsely Szovát <ezselyszovat@gmail.com>
2021-10-17 22:33:56 +0200

    demo project upload

---
+++ demoproject/springboot-backend/src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,26 @@
+package net.javaguides.springboot;
+
+import net.javaguides.springboot.model.User;
+import net.javaguides.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.userRepository.save(new User("János","Nagy","nagy.janos@gmail.com"));
+		this.userRepository.save(new User("Béla","Kiss","kiss.bela@gmail.com"));
+		this.userRepository.save(new User("Ferenc","Kovács","kovacs.ferenc@gmail.com"));
+	}
+}
